624196796e7d61220861e31276bf1f7c0e11518b
Jonathan Kirwan <jonathan.kirwan@outlook.com>
2018-06-21 21:44:55 +0100

    Setup basic home page and card views

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container is-fluid has-background-info">
+    <div class="columns">
+      <div class="column hero">
+        <header>
+            <h1 class="is-size-1-mobile">
+                <img src="./assets/button-bg-o.svg" style="width: 400px;" />
+                <span class="is-invisible">Security Service MI5</span>
+            </h1>
+        </header>
+      </div>
+    </div>
+    <div class="columns is-multiline" id="app">
+        <Card name="Jon" phone="07482 424 884" avatar="128_1.png" />
+        <Card name="Jen" phone="07482 424 884" avatar="128_2.png" />
+        <Card name="Paul" phone="07482 424 884" avatar="128_4.png" />
+        <Card name="Donna" phone="07482 424 884" avatar="128_6.png" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Card
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='column is-6 has-background-primary'>
+    <div class='level'>
+        <div class='tile is-child is-4 has-text-centered-mobile has-text-left-tablet'>
+            <img :alt="`${name} profile picture`" :src="`./assets/${avatar}`">
+        </div>
+        <div class='tile is-vertical is-8 is-parent has-text-centered-mobile has-text-left-tablet'>
+            <header>
+                <h2 class='is-size-4 is-child'>
+                    <strong class='color-pink has-text-weight-semibold'>{{ name }}</strong>
+                </h2>
+            </header>
+            <div class='tile is-child'>
+                <p class='is-size-5 color-light-blue has-text-weight-semibold'>{{ phone }}</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,
+    phone: String,
+    avatar: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
